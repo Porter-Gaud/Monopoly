4765dacd700450e7a083ef089c47d5014a40245d
Hay <chay18@CHay18LP-3.local>
2017-10-12 11:15:19 -0400

    added dice graphics and player selection

--- Monopoly/IntroViewController.swift
+++ Monopoly/IntroViewController.swift
@@ -8,17 +8,55 @@
 
 import UIKit
 
-class IntroViewController: UIViewController {
 
+class IntroViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+    
+    @IBOutlet weak var playerNumberWarning: UILabel!
+    @IBOutlet weak var playerPickerView: UIPickerView!
+    let numbersOfPlayersArray = ["2 Players", "3 Players", "4 Players" ,"5 Players", "6 Players"]
+    var numberOfPlayers : Int = 0
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int
+    {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
+    {
+        return numbersOfPlayersArray[row]
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
+    {
+        return numbersOfPlayersArray.count
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        
+        numberOfPlayers = row + 2
+        
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    @IBAction func startGame(_ sender: UIButton) {
+        
+        if numberOfPlayers >= 2 {
+            
+            performSegue(withIdentifier: "Segue", sender: self)
+            
+        } else {
+            
+            playerNumberWarning.text = "You need to select an amount of players"
+            
+        }
+        
     }
     
 }
