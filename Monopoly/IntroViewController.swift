598672d45ac61b7f375eb2984cc7edf5112c81c1
Tillson Galloway <tillson.galloway@gmail.com>
2017-10-18 09:56:04 -0400

    Fix intro stuff

--- Monopoly/IntroViewController.swift
+++ Monopoly/IntroViewController.swift
@@ -9,48 +9,20 @@
 import UIKit
 
 
-class IntroViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDelegate {
+class IntroViewController: UIViewController {
     
     @IBOutlet weak var playerNumberWarning: UILabel!
     @IBOutlet weak var playerPickerView: UIPickerView!
-    let numbersOfPlayersArray = ["2 Players", "3 Players", "4 Players" ,"5 Players", "6 Players"]
     var numberOfPlayers : Int = 0
     
-    func numberOfComponents(in pickerView: UIPickerView) -> Int
-    {
-        return 1
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String?
-    {
-        return numbersOfPlayersArray[row]
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int
-    {
-        return numbersOfPlayersArray.count
-    }
-    
-    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
-        
-        numberOfPlayers = row + 2
-        
-    }
     override func viewDidLoad() {
         super.viewDidLoad()
         
     }
     
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
     @IBAction func startGame(_ sender: UIButton) {
-        
         if numberOfPlayers >= 2 {
-            
-            performSegue(withIdentifier: "Segue", sender: self)
-            
+            performSegue(withIdentifier: "startGame", sender: self)
         } else {
             
             playerNumberWarning.text = "You need to select an amount of players"
@@ -59,5 +31,25 @@ class IntroViewController: UIViewController, UIPickerViewDataSource, UIPickerVie
         
     }
     
+}
+
+extension IntroViewController: UIPickerViewDataSource, UIPickerViewDelegate {
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return "\(row + 2) Players"
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return 5 // [2, 3, 4, 5, 6] players
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        numberOfPlayers = row + 2
+        
+    }
+
 }
 
